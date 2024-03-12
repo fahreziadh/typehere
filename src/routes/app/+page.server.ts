75236910a6c1306b3ef3a1b7ad8c766708cfb5d8
Fahrezi <fahreziadh@gmail.com>
2024-03-12 23:35:56 +0700

    Fix database command in package.json and update form rendering in +page.svelte

--- src/routes/app/+page.server.ts
+++ src/routes/app/+page.server.ts
@@ -5,13 +5,22 @@ export const load = (async (event) => {
 	const session = await event.locals.auth();
 	const userId = session?.user?.id;
 
-	const listForm = await db.query.form.findMany({
-		where(fields, { eq }) {
-			return eq(fields.authorId, userId ?? '');
-		}
-	});
-
 	return {
-		listForm
+		form: await data.getFormById(),
+		listForm: data.getAllFormByUserId(userId),
 	};
 }) satisfies PageServerLoad;
+
+const data = {
+	getAllFormByUserId: async (userId?: string) => {
+		if (!userId) throw new Error('User not found');
+		return await db.query.form.findMany({
+			where(fields, { eq }) {
+				return eq(fields.authorId, userId ?? '');
+			}
+		});
+	},
+	getFormById: async () => {
+		return await db.query.form.findFirst();
+	}
+};
--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -6,32 +6,44 @@
 	export let data: PageData;
 </script>
 
-<Headers title="List Form"/>
-
+<Headers title="List Form" />
+{JSON.stringify(data.form)}
 <div class="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-	{#each data.listForm as dataForm}
-		<a
-			class="w-full group bg-background rounded-sm transition-all border border-foreground/10 hover:border-foreground/30 active:scale-95 active:opacity-50"
-			href={`/app/form/${dataForm.id}`}
-		>
-			<div class="flex justify-center items-center h-20 text-foreground font-medium md:text-base text-sm">
-				{dataForm.title ? dataForm.title : 'Untitled'}
-			</div>
-			<div class="border-t group-hover:border-foreground/30 transition-all p-2 grid grid-cols-5 text-xs px-4">
-				<div class="col-span-4 place-self-start inline-flex gap-4 opacity-70 group-hover:opacity-100">
-					<span class="inline-flex gap-1 items-center font-medium"
-						><CircleUserRound size={14} />14</span
-					>
-                    <span class="inline-flex gap-1 items-center font-medium"
-						><MessageCircleQuestion size={14} />14</span
-					>
+	{#await data.listForm}
+		Loading...
+	{:then listForm}
+		{#each listForm as dataForm}
+			<a
+				class="w-full group bg-background rounded-sm transition-all border border-foreground/10 hover:border-foreground/30 active:scale-95 active:opacity-50"
+				href={`/app/form/${dataForm.id}`}
+			>
+				<div
+					class="flex justify-center items-center h-20 text-foreground font-medium md:text-base text-sm"
+				>
+					{dataForm.title ? dataForm.title : 'Untitled'}
 				</div>
 				<div
-					class="col-span-1 place-self-end transition-all font-medium opacity-20 group-hover:opacity-100"
+					class="border-t group-hover:border-foreground/30 transition-all p-2 grid grid-cols-5 text-xs px-4"
 				>
-					Detail
+					<div
+						class="col-span-4 place-self-start inline-flex gap-4 opacity-70 group-hover:opacity-100"
+					>
+						<span class="inline-flex gap-1 items-center font-medium"
+							><CircleUserRound size={14} />14</span
+						>
+						<span class="inline-flex gap-1 items-center font-medium"
+							><MessageCircleQuestion size={14} />14</span
+						>
+					</div>
+					<div
+						class="col-span-1 place-self-end transition-all font-medium opacity-20 group-hover:opacity-100"
+					>
+						Detail
+					</div>
 				</div>
-			</div>
-		</a>
-	{/each}
+			</a>
+		{/each}
+	{:catch error}
+		Cannot load list form
+	{/await}
 </div>
