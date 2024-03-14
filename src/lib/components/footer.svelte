b01ca937322917fbd1b57a16df32a4469ccc929d
Fahrezi <fahreziadh@gmail.com>
2024-03-14 11:08:07 +0700

    Update social media links in footer component

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -7,13 +7,13 @@
 	<div class="container max-w-[900px] h-20 flex flex-col md:flex-row justify-between items-center">
 		<div class="text-sm opacity-70">© 2024 Fahrezi Adha - All Right Reserved.</div>
 		<div class="flex flex-row items-center gap-2">
-			<a href="https://twitter.com/fahreziadhaa" target="_blank"
-				><Button size="icon" type="button" variant="outline"
+			<a aria-label="twitter" href="https://twitter.com/fahreziadhaa" target="_blank"
+				><Button name="twitter" size="icon" type="button" variant="outline"
 					><Twitter size={18} fill="black" /></Button
 				>
 			</a>
-			<a href="https://github.com/fahreziadh/typehere" target="_blank">
-				<Button size="icon" type="button" variant="outline"><GithubIcon size={18} /></Button>
+			<a aria-label="github" href="https://github.com/fahreziadh/typehere" target="_blank">
+				<Button name="github" size="icon" type="button" variant="outline"><GithubIcon size={18} /></Button>
 			</a>
 		</div>
 	</div>
