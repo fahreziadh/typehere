1342690d679e446c2a4c2222fd22173b2813198b
Fahrezi <fahreziadh@gmail.com>
2024-03-15 09:49:53 +0700

    Add GitHub token for authentication

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,5 +1,6 @@
 import dayjs from 'dayjs';
 import type { LayoutServerLoad } from './$types';
+import {GITHUB_TOKEN} from "$env/static/private"
 
 export const load: LayoutServerLoad = async ({ locals, platform }) => {
 	let githubStar = 0;
@@ -10,7 +11,9 @@ export const load: LayoutServerLoad = async ({ locals, platform }) => {
 		const lastRevalidate = new Date(lr);
 		const shouldRevalidate = dayjs(lastRevalidate).add(r, 'second').isBefore(dayjs());
 
-		if (shouldRevalidate) {
+		if (!shouldRevalidate) {
+			githubStar = c;
+		} else {
 			githubStar = await getGithubStar();
 			await platform?.env?.myKv.put(
 				'githubStar',
@@ -20,9 +23,17 @@ export const load: LayoutServerLoad = async ({ locals, platform }) => {
 					lr: Date.now()
 				})
 			);
-		} else {
-			githubStar = c;
 		}
+	}else{
+		githubStar = await getGithubStar();
+		await platform?.env?.myKv.put(
+			'githubStar',
+			JSON.stringify({
+				c: githubStar,
+				r: 60 * 60, // 1 hour
+				lr: Date.now()
+			})
+		);
 	}
 	return {
 		session: await locals.auth(),
@@ -31,7 +42,20 @@ export const load: LayoutServerLoad = async ({ locals, platform }) => {
 };
 
 async function getGithubStar() {
-	const res = await fetch('https://api.github.com/repos/fahreziadh/typehere');
+	if (!GITHUB_TOKEN) {
+		return 0;
+	}
+
+
+	const res = await fetch('https://api.github.com/repos/fahreziadh/typehere', {
+		headers:{
+			"Accept": "application/vnd.github+json",
+			'X-GitHub-Api-Version': '2022-11-28',
+			"User-Agent": "fahreziadh",
+			'Authorization': `token ${GITHUB_TOKEN}`
+		}
+	});
+	console.log(res.status, res.statusText)
 	const data = await res.json();
 	return data.stargazers_count as number;
 }
